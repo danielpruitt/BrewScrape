d52945dbcfe9e2140d92ccb48cd429ee4dfdad33
danielpruitt <35474097+danielpruitt@users.noreply.github.com>
2018-09-20 18:24:43 -0400

    doesn't add duplicate articles

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,108 +1,131 @@
-// var cheerio = require("cheerio");
-// var request = require("request");
+var cheerio = require("cheerio");
+var request = require("request");
 
-// var db = require("../models/index");
+var db = require("../models/index");
 
-// //scraper 
-// module.exports = function (app) {
-//     app.get("/scrape", function (req, res) {
-//         request("https://www.brewbound.com/", function (err, response, html) {
-//             //call cheerio with a $
-//             var $ = cheerio.load(html);
+//scraper 
+module.exports = function (app) {
+    // app.get("/api/fetch", function (req, res) {
+    //     request("https://www.brewbound.com/", function (err, response, html) {
+    //         //call cheerio with a $
+    //         var $ = cheerio.load(html);
 
-//             //select the element from the body, brewbound uses a h3 tag to contain the title and link
-//             $("h3").each(function (i, element) {
-//                 var result = [];
-//                 var title = $(element).children().text();
-//                 var link = $(element).children().attr("href")
+    //         //select the element from the body, brewbound uses a h3 tag to contain the title and link
+    //         $("article.has-image").each(function (i, element) {
+    //             var results = [];
+    //             var $div = $(element).find("h3");
+    //             var title = $div.children().text();
+    //             var link = $div.children().attr("href")
 
-            
-//                     result.push({
-//                         title: title,
-//                         link: link
-//                     });
+    //             var $divSum = $(element).find("div.summary");
+    //             var summary = $divSum.children().text();
 
-                
+    //             var $divImg = $(element).find("picture");
+    //             var img = $divImg.children().attr("srcset");
+
+    //             //results array will be pushed to the database
+    //             results.push({
+    //                 title: title,
+    //                 link: link,
+    //                 img: img,
+    //                 summary: summary
+    //             });
+
+    //             db.Article.findOne({title: results.title}).then(function(dbArticle) {
+    //                 if(dbArticle) {
+    //                     console.log("Already exists in database");
+
+    //                 } else {
+    //                     db.Article.create(results).then(function(dbArticle) {
+    //                         console.log(dbArticle);
+    //                     });
+    //                 };
+    //             });
+
+    //             //create each object in Results in the database
+    //             // db.Article.create(results)
+    //             //     .then(function (dbArticle) {
+    //             //         console.log(dbArticle)
+    //             //         res.json(dbArticle)
+    //             //     });
+    //         });
+    //         res.send("scraped")
+    //     });
+    // });
+
+    app.get("/articles", function (req, res) {
+        db.Article.find({})
+            .then(function (dbArticle) {
+                res.json(dbArticle)
+            })
+            .catch(function (err) {
+                res.json(err);
+            });
+    });
+
+    app.get("/articles/:id", function (req, res) {
+        db.Article.findOne({ _id: req.params.id })
+            .populate("note")
+            .then(function (dbArticle) {
+                res.json(dbArticle);
+            })
+            .catch(function (err) {
+                res.json(err);
+            });
+    });
+
+    app.post("/articles/:id", function (req, res) {
+        db.Note.create(req.body)
+            .then(function (dbNote) {
+                return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true })
+            })
+            .then(function (dbArticle) {
+                res.json(dbArticle)
+            })
+            .catch(function (err) {
+                res.json(err)
+            });
+    });
+
+    app.get("/api/fetch", function (req, res) {
+        request("https://www.brewbound.com/", function (err, response, html) {
+            //call cheerio with a $
+            var $ = cheerio.load(html);
+
+            //select the element from the body, brewbound uses a h3 tag to contain the title and link
+            $("article.has-image").each(function (i, element) {
+                var results = {};
+                var $div = $(element).find("h3");
+                var title = $div.children().text();
+                var link = $div.children().attr("href")
+
+                var $divSum = $(element).find("div.summary");
+                var summary = $divSum.children().text();
+
+                var $divImg = $(element).find("picture");
+                var img = $divImg.children().attr("srcset");
 
 
-//                 db.Article.create(result)
-//                     .then(function (dbArticle) {
-//                         console.log(dbArticle)
-//                     })
-//                     .catch(function (err) {
-//                         return res.json(err)
-//                     })
-//             });
+                //results object will be pushed to the database
+                results.title = title;
+                results.link = link;
+                results.summary = summary;
+                results.img = img;
 
-//         });
-//         res.send("Scrape Complete")
-//     });
+                db.Article.findOne({ title: results.title }).then(function (dbArticle) {
+                    if (dbArticle) {
+                        console.log("Already exists in database");
 
-//     // app.get("/scrape", function (req, res) {
-//     //     request("https://www.brewbound.com/", function (err, response, html) {
-//     //         //call cheerio with a $
-//     //         var $ = cheerio.load(html);
-
-//     //         //select the element from the body, brewbound uses a h3 tag to contain the title and link
-//     //         $("h3").each(function (i, element) {
-//     //             var result = [];
-//     //             var title = $(element).children().text();
-//     //             var link = $(element).children().attr("href")
-
-            
-//     //                 result.push({
-//     //                     title: title,
-//     //                     link: link
-//     //                 });
-
-                
+                    } else {
+                        db.Article.create(results).then(function (dbArticle) {
+                            console.log(dbArticle);
+                        });
+                    };
+                });
+            });
+            res.json("scraped");
+        });
+    });
 
 
-//     //             db.Article.create(result)
-//     //                 .then(function (dbArticle) {
-//     //                     console.log(dbArticle)
-//     //                 })
-//     //                 .catch(function (err) {
-//     //                     return res.json(err)
-//     //                 })
-//     //         });
-
-//     //     });
-//     //     res.send("Scrape Complete")
-//     // });
-
-//     app.get("/articles", function (req, res) {
-//         db.Article.find({})
-//             .then(function (dbArticle) {
-//                 res.json(dbArticle)
-//             })
-//             .catch(function (err) {
-//                 res.json(err);
-//             });
-//     });
-
-//     app.get("/articles/:id", function (req, res) {
-//         db.Article.findOne({ _id: req.params.id })
-//             .populate("note")
-//             .then(function (dbArticle) {
-//                 res.json(dbArticle);
-//             })
-//             .catch(function (err) {
-//                 res.json(err);
-//             });
-//     });
-
-//     app.post("/articles/:id", function (req, res) {
-//         db.Note.create(req.body)
-//             .then(function (dbNote) {
-//                 return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true })
-//             })
-//             .then(function (dbArticle) {
-//                 res.json(dbArticle)
-//             })
-//             .catch(function (err) {
-//                 res.json(err)
-//             });
-//     });
-
-// }
+}
--- server.js
+++ server.js
@@ -3,8 +3,6 @@ var exphbs = require("express-handlebars");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
-var cheerio = require("cheerio");
-var request = require("request");
 
 var PORT = process.env.PORT || 3005;
 
@@ -33,85 +31,8 @@ mongoose.connect(MONGODB_URI);
 
 
 // routes
-// require("./routes/apiRoutes")(app);
+require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
-
-
-
-//scraper 
-// can probably delete this, once I get handlebars working
-
-app.get("/api/fetch", function (req, res) {
-    request("https://www.brewbound.com/", function (err, response, html) {
-        //call cheerio with a $
-        var $ = cheerio.load(html);
-
-        //select the element from the body, brewbound uses a h3 tag to contain the title and link
-        $("article.has-image").each(function (i, element) {
-            var results = [];
-            var $div = $(element).find("h3");
-            var title = $div.children().text();
-            var link = $div.children().attr("href")
-
-            var $divSum = $(element).find("div.summary");
-            var summary = $divSum.children().text();
-
-            var $divImg = $(element).find("picture");
-            var img = $divImg.children().attr("srcset");
-
-            //results array will be pushed to the database
-            results.push({
-                title: title,
-                link: link,
-                img: img,
-                summary: summary
-            });
-
-            //create each object in Results in the database
-            db.Article.create(results)
-                .then(function (dbArticle) {
-                    console.log(dbArticle)
-                    res.json(dbArticle)
-                });
-        });
-        res.send("scraped")
-    });
-});
-
-app.get("/articles", function (req, res) {
-    db.Article.find({})
-        .then(function (dbArticle) {
-            res.json(dbArticle)
-        })
-        .catch(function (err) {
-            res.json(err);
-        });
-});
-
-app.get("/articles/:id", function (req, res) {
-    db.Article.findOne({ _id: req.params.id })
-        .populate("note")
-        .then(function (dbArticle) {
-            res.json(dbArticle);
-        })
-        .catch(function (err) {
-            res.json(err);
-        });
-});
-
-app.post("/articles/:id", function (req, res) {
-    db.Note.create(req.body)
-        .then(function (dbNote) {
-            return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true })
-        })
-        .then(function (dbArticle) {
-            res.json(dbArticle)
-        })
-        .catch(function (err) {
-            res.json(err)
-        });
-});
-
 
 
 // Start the server
